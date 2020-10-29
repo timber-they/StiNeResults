c22f5b348bf9ddcd8d59c14510abbeae19c78cbb
MetaColon <timo@teemze.de>
2020-10-29 20:46:40 +0100

    Cleanup, better config, more robust

---
+++ StiNeResults/Config.cs
@@ -0,0 +1,8 @@
+namespace StiNeResults
+{
+    public struct Config
+    {
+        public string User;
+        public string Password;
+    }
+}
--- StiNeResults/Program.cs
+++ StiNeResults/Program.cs
@@ -2,103 +2,266 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+
 
 namespace StiNeResults
 {
     class Program
     {
-        static void Main(string[] args)
+        private static FirefoxDriver _driver;
+        private const  int           TRIES = 10;
+
+        static void Main (string [] args)
         {
-            var driver = new FirefoxDriver("./");
-            driver.Navigate().GoToUrl("https://www.stine.uni-hamburg.de/");
-            if (!driver.Title.StartsWith("Universität Hamburg"))
-            {
-                Console.Error.WriteLine($"Page called '{driver.Title}', won't proceed");
+            _driver = new FirefoxDriver ("./");
+
+            if (!Init ())
                 return;
-            }
-
-            var user = driver.FindElement(By.Id("field_user"));
-            if (user == null)
-            {
-                Console.Error.WriteLine("User field not found");
+            if (!Login ())
                 return;
-            }
-            user.SendKeys("baw8431");
-
-            var password = driver.FindElement(By.Id("field_pass"));
-            if (password == null)
-            {
-                Console.Error.WriteLine("Password field not found");
+            if (!Navigate ())
                 return;
-            }
 
-            string pwd;
-            if (File.Exists("password"))
-                pwd = File.ReadAllText("password");
-            else
+            AnalyzeErgebnisse ();
+        }
+
+        private static bool Init ()
+        {
+            for (var i = 0; i < TRIES; i++)
             {
-                Console.WriteLine("Enter password");
-                pwd = Console.ReadLine();
-            }
-            password.SendKeys(pwd);
-
-            var login = driver.FindElement(By.Id("logIn_btn"));
-            if (login == null)
-            {
-                Console.Error.WriteLine("Login button not found");
-                return;
-            }
-            login.Click();
-
-            Console.WriteLine("Authenticated");
-
-            var studium = driver.FindElement(By.LinkText("Studium"));
-            if (studium == null)
-            {
-                Console.Error.WriteLine("Studium tab not found");
-                return;
-            }
-            studium.Click();
-
-            var prüfungsergebnisse = driver.FindElement(By.LinkText("Prüfungsergebnisse"));
-            if (prüfungsergebnisse == null)
-            {
-                Console.Error.WriteLine("Prüfungsergebnisse button not found");
-                return;
-            }
-            prüfungsergebnisse.Click();
-
-            Console.WriteLine("On tab");
-
-            List<IWebElement> ergebnisse;
-            do
-            {
-                ergebnisse = driver.FindElements(By.TagName("tr")).Skip(1).ToList();
-                Console.WriteLine($"Got {ergebnisse.Count} Ergebnisse");
-                foreach (var ergebnis in ergebnisse)
+                try
                 {
-                    var cols = ergebnis.FindElements(By.TagName("td"));
-                    if (cols.Count != 5)
+                    _driver.Navigate ().GoToUrl ("https://www.stine.uni-hamburg.de/");
+                    if (_driver.Title.StartsWith ("Universität Hamburg"))
                     {
-                        Console.Error.WriteLine($"Invalid column count: {cols.Count}");
-                        continue;
+                        Console.WriteLine ("Initialized");
+                        return true;
                     }
 
-                    Console.WriteLine($"Ergebnis für '{cols[0].Text}': {cols[2].Text}");
+                    Console.Error.WriteLine ($"Page called '{_driver.Title}'");
                 }
-                driver.Navigate().Refresh();
-            } while (ergebnisse.Count == 3);
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine ($"Init threw an exception: {e}, {e.StackTrace}");
+                }
 
-            Console.WriteLine("Got new Ergebnis!");
-            Console.Beep(440, 10000);
+                Console.Error.WriteLine ($"Couldn't init, try {i + 1}/{TRIES}");
+            }
+
+            Console.Error.WriteLine ("Couldn't init");
+
+            return false;
+        }
+
+        private static bool Login ()
+        {
+            for (var i = 0; i < TRIES; i++)
+            {
+                if (LoginTry ())
+                {
+                    Console.WriteLine ("Authenticated");
+                    return true;
+                }
+
+                Console.Error.WriteLine ($"Couldn't authenticate, try {i + 1}/{TRIES}");
+                try
+                {
+                    _driver.Navigate ().Refresh ();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine ($"Refresh threw an exception: {e}, {e.StackTrace}");
+                    return false;
+                }
+
+                DismissDialog ();
+            }
+
+            return false;
+
+            static bool LoginTry ()
+            {
+                try
+                {
+                    var config = GetConfig ();
+
+                    var user = _driver.FindElement (By.Id ("field_user"));
+                    if (user == null)
+                    {
+                        Console.Error.WriteLine ("User field not found");
+                        return false;
+                    }
+
+                    user.SendKeys (config.User);
+
+                    var password = _driver.FindElement (By.Id ("field_pass"));
+                    if (password == null)
+                    {
+                        Console.Error.WriteLine ("Password field not found");
+                        return false;
+                    }
+
+                    password.SendKeys (config.Password);
+
+                    var login = _driver.FindElement (By.Id ("logIn_btn"));
+                    if (login == null)
+                    {
+                        Console.Error.WriteLine ("Login button not found");
+                        return false;
+                    }
+
+                    login.Click ();
+                    if (_driver.FindElements (By.Id ("field_user")).Count == 0)
+                        return true;
+
+                    Console.Error.WriteLine ("Login unsuccessful");
+                    return false;
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine ($"Login threw an exception: {e}, {e.StackTrace}");
+                    return false;
+                }
+            }
+        }
+
+        private static void DismissDialog ()
+        {
+            try
+            {
+                if (_driver.SwitchTo ().Alert () != null)
+                    _driver.SwitchTo ().Alert ().Accept ();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine ($"Dismissing the alert threw an exception: {e}, {e.StackTrace}");
+            }
+        }
+
+        private static Config GetConfig ()
+        {
+            if (File.Exists ("config"))
+            {
+                var content = File.ReadAllLines ("config");
+                if (content.Length != 2)
+                    Console.Error.WriteLine ("Invalid config file");
+                else
+                    return new Config {User = content [0], Password = content [1]};
+            }
+
+            Console.WriteLine ("Enter username");
+            var username = Console.ReadLine ();
+            Console.WriteLine ("Enter password");
+            var password = Console.ReadLine ();
+
+            return new Config {User = username, Password = password};
+        }
+
+        private static bool Navigate ()
+        {
+            for (var i = 0; i < TRIES; i++)
+            {
+                if (NavigateTry ())
+                {
+                    Console.WriteLine ("On tab");
+                    return true;
+                }
+
+                Console.WriteLine ($"Navigate failed, try {i + 1}/{TRIES}");
+                try
+                {
+                    _driver.Navigate ().Refresh ();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine ($"Refresh threw an exception: {e}, {e.StackTrace}");
+                    return false;
+                }
+            }
+
+            Console.WriteLine ("Navigate failed");
+            return false;
+
+            static bool NavigateTry ()
+            {
+                try
+                {
+                    var studium = _driver.FindElement (By.LinkText ("Studium"));
+                    if (studium == null)
+                    {
+                        Console.Error.WriteLine ("Studium tab not found");
+                        return false;
+                    }
+
+                    studium.Click ();
+
+                    var prüfungsergebnisse = _driver.FindElement (By.LinkText ("Prüfungsergebnisse"));
+                    if (prüfungsergebnisse == null)
+                    {
+                        Console.Error.WriteLine ("Prüfungsergebnisse button not found");
+                        return false;
+                    }
+
+                    prüfungsergebnisse.Click ();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine ($"Navigate threw an exception: {e}, {e.StackTrace}");
+                    return false;
+                }
+            }
+        }
+
+        private static void AnalyzeErgebnisse ()
+        {
+            int?               initialErgebnisCount = null;
+            List <IWebElement> ergebnisse           = null;
+            do
+                try
+                {
+                    ergebnisse           =   _driver.FindElements (By.TagName ("tr")).Skip (1).ToList ();
+                    if (ergebnisse.Count == 0)
+                        throw new Exception ("Invalid tab");
+                    initialErgebnisCount ??= ergebnisse.Count;
+                    Console.WriteLine ($"Got {ergebnisse.Count} Ergebnisse");
+                    foreach (var cols in ergebnisse.Select (ergebnis => ergebnis.FindElements (By.TagName ("td"))))
+                    {
+                        if (cols.Count != 5)
+                        {
+                            Console.Error.WriteLine ($"Invalid column count: {cols.Count}");
+                            continue;
+                        }
+
+                        Console.WriteLine ($"Ergebnis für '{cols [0].Text}': {cols [2].Text}");
+                    }
+
+                    _driver.Navigate ().Refresh ();
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine ($"Ergebnisse threw an exception: {e}, {e.StackTrace}");
+                    if (!(Init () && Login () && Navigate ()))
+                        return;
+                }
+            while (ergebnisse == null || ergebnisse.Count == initialErgebnisCount);
+
+            Console.WriteLine ("Got new Ergebnis!");
+            Notify ();
+        }
+
+        private static void Notify ()
+        {
+            Console.Beep (440, 10000);
             while (true)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("█");
+                Console.Write ("█");
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("█");
+                Console.Write ("█");
             }
         }
     }
